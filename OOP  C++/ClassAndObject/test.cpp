1225353e7e8cffecd1c57ab2a02d709b343e9cea
duongpham26 <duong.pham2617@gmail.com>
2024-04-29 12:37:43 +0700

    OOP-c++ objectClass test.cpp add distructor

---
+++ OOP  C++/ClassAndObject/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+
+class test{
+public:
+   int num;
+   test(int);
+   ~test();
+};
+
+test::test(int i) {
+   num = i;
+   std::cout << "++ Goi ham thiet lao coi num = " << num << "\n";
+}
+
+test::~test() {
+   std::cout << "--Huy ham thiet lap voi num = " << num << "\n";
+}
+
+
+
+int main() {
+   void fct(int);
+   test t1(1);
+   for (int i = 0; i < 2; ++i) {
+      fct(i);
+   }
+   return 0;
+}
+
+void fct(int p) {
+   test t(2*p);
+}
