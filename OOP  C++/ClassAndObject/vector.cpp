85a7e36621ca57479dd0517e962d41ee4430a0a9
duongpham26 <duong.pham2617@gmail.com>
2024-04-29 15:37:51 +0700

    OOP-c++ objectClass vector.cpp add copy-constructor

--- OOP  C++/ClassAndObject/vector.cpp
+++ OOP  C++/ClassAndObject/vector.cpp
@@ -9,6 +9,7 @@ public:
    vector();
    vector(int size);
    vector(int size, float *a);
+   vector(const vector &);
    void display();
    ~vector();
 };
@@ -40,6 +41,15 @@ vector::vector(int size, float *a) {
    }
 }
 
+vector::vector(const vector &b) {
+   std::cout << "\nSu dung ham thiet lap sao chep\n";
+   n = b.n;
+   v = new float [n];
+   for(int i = 0; i < n; ++i) {
+      v[i] = b.v[i];
+   }
+}
+
 void vector::display() {
    std::cout << "Doi tuong tai: " << this << "\n";
    std::cout << " n = " << n << " - ";
@@ -60,5 +70,8 @@ int main() {
    float b[] = {1,2,3,9};
    vector v1(4, b);
    v1.display();
+
+   vector v2(v1);
+   v2.display();
    return 0;
 }
