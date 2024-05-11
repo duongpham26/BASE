e8e3f91838e51cf293d111c3e15149255d57c1ff
duongpham26 <duong.pham2617@gmail.com>
2024-05-11 23:39:41 +0700

    DataStructuresAndAlgorithms BaiTapLonCaNhan GiaPhaGiaDinh.cpp done

--- DataStructuresAndAlgorithms/BaiTapLonCaNhan/GiaPhaGiaDinh.cpp
+++ DataStructuresAndAlgorithms/BaiTapLonCaNhan/GiaPhaGiaDinh.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string.h>
 #include <conio.h>
+#include <iomanip>
 using namespace std;
 
 
@@ -14,26 +15,13 @@ struct person {
 struct node
 {
    person data;
-   node *father;
+   node *parent;
    node *chilren;
    node *siblings;
 };
 
-
-// struct DList
-// {
-//    node *first;
-//    node *last;
-// };
-
-// void Init(DList &l)
-// {
-//    l.first = l.last = NULL;
-// }
-
 node *greatPersonNode(person data)
 {
-   // node *p = (node *)malloc(sizeof(node));
    node *personNode = new node;
 
    if (personNode == NULL)
@@ -42,70 +30,126 @@ node *greatPersonNode(person data)
       return NULL;
    }
    personNode->data = data;
-   personNode->father = NULL;
+   personNode->parent = NULL;
    personNode->chilren = NULL;
    personNode->siblings = NULL;
    return personNode;
 }
 
-void addChild(node *fatherNode, person data) {
+void addChild(node *parentNode, person data) {
    node *childNode = greatPersonNode(data);
-   if(fatherNode->chilren == NULL) {// chua co con
-      fatherNode->chilren = childNode;
-      childNode->father = fatherNode;
+   if(parentNode->chilren == NULL) {// chua co con
+      parentNode->chilren = childNode;
+      childNode->parent = parentNode;
    } else {
-      node *indexNode = fatherNode->chilren;
+      node *indexNode = parentNode->chilren;
       while(indexNode->siblings != NULL) {
          indexNode = indexNode->siblings;
       }
       indexNode->siblings = childNode;
-      childNode->father = fatherNode;
+      childNode->parent = parentNode;
    }
 }
 
 person inputPerson() {
    person data;
-   cout << "Nhap ID: "; cin >> data.ID;
    cin.clear();
    fflush(stdin);
-   cout << "Nhap Name: "; cin >> data.name; 
+   cout << "Enter ID: "; cin >> data.ID;
+   cin.clear();
+   fflush(stdin);
+   cout << "Enter Name: "; gets(data.name);
+
    cin.clear();
    fflush(stdin); 
-   cout << "Nhap Tuoi: "; cin >> data.yearofBirth;
+   cout << "Enter Year Of Birth: "; cin >> data.yearofBirth;
    return data;
 }
 
-// node addRootNode() {
-//    root = greatPersonNode(inputPerson());
-//    return root;
-//    cout << "test " <<root->data.ID;
-// }
-
 void printPerSon(node *personNode) {
-   cout << "ID: " << personNode->data.ID << "\n";
-   cout << "Name: " << personNode->data.name << "\n";
-   cout << "Year Of Birth: " << personNode->data.yearofBirth << "\n";
+   cout << "\n";
+   cout << setw(10) << left << personNode->data.ID;
+   cout << setw(25) << left << personNode->data.name;
+   cout << setw(25) << left << personNode->data.yearofBirth;
+}
+
+void printParentAndChilren(node *parentNode) {
+   cout <<"-------------------------------------------------\n";
+   cout <<"---parent-----\n";
+   cout << setw(10) << left <<  "ID";
+   cout << setw(25) << left << "Name";
+   cout << setw(25) << left << "Year Of Birth";
+   printPerSon(parentNode);
+   cout <<"\n\n---CHILDREN-----\n";
+   cout << setw(10) << left <<  "ID";
+   cout << setw(25) << left << "Nam";
+   cout << setw(25) << left << "Year Of Birth";
+   node *indexNode = parentNode->chilren;
+   while(indexNode != NULL) {
+      printPerSon(indexNode);
+      indexNode = indexNode->siblings;
+   }
+   cout <<"\n-------------------------------------------------\n\n";
+}
+
+node *search(node *rootNode, int ID) {
+   if(rootNode != NULL) {
+      if(rootNode->data.ID == ID)
+			return rootNode;
+		else if (rootNode->data.ID > ID)
+			return search(rootNode->chilren, ID);
+		else return search(rootNode->siblings, ID);
+   }
+   return NULL;
+}
+
+void addChildForParent(node *rootNode) {
+   int ID;
+   node *searchNodeParent;
+   do {
+      cout << "Enter the ID of the parent who wants to add the child: "; cin >> ID;
+      searchNodeParent = search(rootNode, ID);
+      if(searchNodeParent == NULL) cout << "Not found, please re-enter ID!\n";
+   } while(search(rootNode, ID) == NULL);
+   addChild(searchNodeParent, inputPerson());
+}
+
+void searchPrintParentAndChilren(node *rootNode) {
+   int ID;
+   node *searchNodeParent;
+   do {
+      cout << "Enter the ID of the parent who wants to print information: "; cin >> ID;
+      searchNodeParent = search(rootNode, ID);
+      if(searchNodeParent == NULL) cout << "Not found, please re-enter ID!\n";
+   } while(search(rootNode, ID) == NULL);
+   printParentAndChilren(searchNodeParent);
 }
 
 
 
 int main()
 {
-   node *root;
+   node *root; //nut goc
+
+   //tao thanh vien moi ID = 10
+   cout << "Enter information for root.\n";
    root = greatPersonNode(inputPerson());
 
-   printPerSon(root);
+   //them con cho node cha ID = 10
+   addChildForParent(root); //ID = 7
+   addChildForParent(root); //ID = 8
+   addChildForParent(root); //ID = 9
 
-   addChild(root, inputPerson());
-   printPerSon(root->chilren);
+   //them con cho node cha ID = 7
+   addChildForParent(root); // ID = 5
+   addChildForParent(root); // ID = 6
 
-   cout << "\nthem 1 con nua--------------";
-   addChild(root, inputPerson());
-   printPerSon(root->chilren->siblings);
+   cout <<"\n";
+   // in ra node ID = 10 va cac con cua no
+   searchPrintParentAndChilren(root);
 
-
-
-   
+   // in ra node ID = 7 va cac con cua no
+   searchPrintParentAndChilren(root);
 
    return 0;
 }
