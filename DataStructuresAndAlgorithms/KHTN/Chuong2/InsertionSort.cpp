4c323fb0ea30724e0ae1650edc56aa0a5c67649b
duongpham26 <duong.pham2617@gmail.com>
2024-05-23 15:17:04 +0700

    DataStructuresAndAlgoritms KHTN/chuong2 add InsetionSort1_2_3

--- DataStructuresAndAlgorithms/KHTN/Chuong2/InsertionSort.cpp
+++ DataStructuresAndAlgorithms/KHTN/Chuong2/InsertionSort.cpp
@@ -12,12 +12,46 @@ void InsertionSort1(int *a, int n) {
    }
 }
 
-int main() {
-   const int n = 10;
-   int a[n];
-   randArray(a, n, 1, 50);
+void insertionSort2_sentinel(int *a, int n) {
+   int i, j , v;
+   for(i = 1; i <= n; ++i) {
+      v = a[i]; j = i - 1;
+      a[-1] = v;
+      while(v < a[j]) {
+         a[j+1] = a[j];
+         j--;
+      }
+      a[j+1] = v;
+   }
+}
 
-   InsertionSort1(a,n);
+void BinaryInsertionSort3(int *a, int n) {
+   int i, j, v;
+   int mid, left, right;
+   for(i = 1; i < n; ++i) {
+      v = a[i]; 
+      a[-1] = v;
+      left = 0; right = i - 1;
+      while(left <= right) {
+         mid = (left + right) / 2;
+         cout << "Mid: "  << mid << "\n";
+         if(v < a[mid]) right = mid - 1;
+         else left = mid + 1;
+      }
+      for(j = i - 1; j >= left; j--) {
+         a[j+1] = a[j];
+      }
+      a[left] = v;
+   }
+}
+
+int main() {
+   const int n = 5;
+   int a[n] = {1,19,20,21,18};
+
+   // randArray(a, n, 1, 10);
+
+   BinaryInsertionSort3(a,n);
    printArray(a,n);
    return 0;
 }
