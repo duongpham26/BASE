1c6776efd313f48e541784f6cb77fb6ca2212c18
duongpham26 <duong.pham2617@gmail.com>
2024-06-25 22:40:09 +0700

    add fetchjs

--- JS/f8/JSON_FETCH_POSTMAN/main.js
+++ JS/f8/JSON_FETCH_POSTMAN/main.js
@@ -188,23 +188,241 @@ var promise = new Promise( // trả về đối tượng
 //       console.log(error) 
 //    })
 
-var promise1 = new Promise(function(resolve, reject) {
-   setTimeout(function() {
-      resolve([1, 2]);
-   }, 2000);
-});
+// var promise1 = new Promise(function(resolve, reject) {
+//    setTimeout(function() {
+//       resolve([1, 2]);
+//    }, 2000);
+// });
 
-var promise2 = new Promise(function(resolve, reject) {
-   setTimeout(function() {
-      resolve([3])
-   }, 5000);
-})
+// var promise2 = new Promise(function(resolve, reject) {
+//    setTimeout(function() {
+//       resolve([3])
+//    }, 5000);
+// })
 
-Promise.all([promise1, promise2])
-   .then(function([result1, result2]) {
-      console.log(result1.concat(result2));
+// Promise.all([promise1, promise2])
+//    .then(function([result1, result2]) {
+//       console.log(result1.concat(result2));
+   // })
+
+//    const users = [
+//       {
+//          id: 1,
+//          name: "Duong Pham",
+//       },
+//       {
+//          id: 2,
+//          name: "Bích Duy",
+//       }
+//    ]
+// const comments = [
+//    {
+//       id: 1,
+//       user_id: 1,
+//       content: "Co di choi hong?"
+//    },
+//    {
+//       id: 2,
+//       user_id: 2,
+//       content: "Co."
+//    },
+//    {
+//       id: 3,
+//       user_id: 1,
+//       content: "HEHE."
+//    },
+// ]
+
+// const getComments = function() {
+//    return new Promise(function(resolve, reject) {
+//       setTimeout(function() {
+//          resolve(comments)
+//       }, 2000);
+//    });
+// };
+// const getUsers = function(userId) {
+//    return new Promise(function(resolve, reject) {
+//       const results = users.filter(function(user) {
+//          return userId.includes(user.id);
+//       })
+//       setTimeout(function() {
+//          resolve(results)
+//       }, 2000);
+//    });
+// };
+
+// getComments(comments)
+//    .then(function(data) {
+//       var userId = data.map(function(comment) {
+//       return comment.user_id;
+//    });
+//    return getUsers(userId)
+//       .then(function(users) {
+//          return {
+//             users: users,
+//             comments: comments,
+//          }
+//       })
+//    .then(function(data) {
+//       const ulElement = document.getElementById('box-comment');
+//       let xhtml = '';
+//       data.comments.forEach(function(comment) {
+//          let findUser = data.users.find(function(user) {
+//             return comment.user_id === user.id;
+//          });
+//          xhtml += `<li>${findUser.name}: ${comment.content}</li>`
+//       });
+//       ulElement.innerHTML = xhtml;
+//       console.log(ulElement);
+//    })
+// });
+
+
+// fetch ===================
+// JSON server: fake API server / mock API   
+// return một promise 
+// response.json() => phương thức trả về giống như JSON.parser()
+// fetch("https://jsonplaceholder.org/posts")
+//    .then(function(response) {
+//       return response.json(); // json.parser()
+//    })
+//    .then(function(posts) {
+//       const ulelement = document.getElementById('box-comment');
+//       let xhmtl = '';
+//       posts.forEach(function(post) {
+//          xhmtl += `
+//          <li>
+//             <h2>${post.title}</h2>   
+//             <p>${post.content}</p>
+//          </li>
+//          `
+//       });
+//       ulelement.innerHTML = xhmtl;
+//    })
+
+// fetch('http://localhost:3000/posts')
+//    .then(function(response) {
+//       return response.json();
+//    })
+//    .then(function(posts) {
+//       console.log(posts);
+//    })
+
+
+// example ====
+
+
+let urlAPI = "http://localhost:3000/courses";
+
+function start() {
+   getCourses(renderCourses);
+   handleCreateForm();
+}
+start();
+
+function getCourses(callback) {
+   fetch(urlAPI)
+         .then(function(response) {
+            return response.json();
+         })
+         .then(callback);
+}
+
+function createCourse(data, callback) {
+   fetch(urlAPI, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(data)
    })
-      
+   .then(response => response.json())
+   .then(callback) 
+}
+
+function handleDelete(id) {
+   fetch(urlAPI + '/' + id, {
+         method: 'DELETE',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+   })
+   .then(response => response.json())
+   .then(function(data) {
+      let liElement = document.querySelector(`.data-id-${data.id}`)
+      if(liElement) liElement.remove();
+   }) 
+}
+
+function update(data, id, callback) {
+   fetch(urlAPI + '/' + id, {
+      method: 'PUT',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+   })
+   .then(response => response.json())
+   .then(callback)
+}
+
+function handleUpdate(id) {
+   var title = document.querySelector('input[name="name"]');
+   var content = document.querySelector('input[name="content"]');
+   let liElement = document.querySelector(`.data-id-${id}`)
+   title.value = liElement.querySelector('h4').innerText;
+   content.value = liElement.querySelector('p').innerText;
+   var btn = document.querySelector("#btn");
+   btn.innerText = "Update";
+   
+   btn.onclick = function(e) {
+      let data = {
+         title: title.value,
+         content: content.value
+      };
+      update(data, id , function (data) {
+         liElement.querySelector('h4').innerText = data.title;;
+         liElement.querySelector('p').innerText = data.content;
+         btn.innerText = "Submit";
+         title.value = "";
+         content.value = "";
+      });
+   } 
+}
+
+function renderCourses(courses) {
+   var listCoursesBlock = document.querySelector("#box-comment");
+   var htmls = courses.map(course => {
+         return `
+         <li class="data-id-${course.id}">
+            <h4>${course.title}</h4>
+            <p>${course.content}</p>
+            <button onclick="handleDelete('${course.id}')">Xóa</button>
+            <button onclick="handleUpdate('${course.id}')">Update</button>
+         </li>
+         `;
+   });
+   listCoursesBlock.innerHTML = htmls.join('');
+}
+
+function handleCreateForm() {
+   var btn = document.querySelector("#btn");
+   btn.onclick = function(event) {
+         event.preventDefault(); // Ngăn chặn form reload lại trang
+         var title = document.querySelector('input[name="name"]').value;
+         var content = document.querySelector('input[name="content"]').value;
+         var formData = {
+            title,
+            content,
+         };
+         createCourse(formData, function() {
+         getCourses(renderCourses);
+         });
+   }
+}
+
+
+
    
 
 
