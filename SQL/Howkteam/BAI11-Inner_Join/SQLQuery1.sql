ceb718f858105f2376f65956165a09798d07220b
duongpham26 <duong.pham2617@gmail.com>
2024-10-16 00:57:36 +0700

    add sql

---
+++ SQL/Howkteam/BAI11-Inner_Join/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿USE HowKteam
+GO
+
+-- inner join kieu cu
+SELECT * FROM dbo.BOMON, dbo.GIAOVIEN
+WHERE GIAOVIEN.MABM = BOMON.MABM
+
+
+-- nên viết kiểu này, kiểu mới và đúng chuẩn (giống a giao b) ( INNER JOIN  + ON = JOIN + ON)
+SELECT * FROM dbo.GIAOVIEN INNER JOIN dbo.BOMON 
+ON BOMON.MABM = GIAOVIEN.MABM
