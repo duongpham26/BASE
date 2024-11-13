2cd075416e3cdd97cfdbd96db786dbe7d63d6e4a
duongpham26 <duong.pham2617@gmail.com>
2024-11-13 21:47:55 +0700

    SpingMVC chapter7 AutoConfig-Security

--- Java/SpringMVC/HoiDanIt/LaptopShop/src/main/java/com/duongpham26/LaptopShop/LaptopShopApplication.java
+++ Java/SpringMVC/HoiDanIt/LaptopShop/src/main/java/com/duongpham26/LaptopShop/LaptopShopApplication.java
@@ -2,8 +2,9 @@ package com.duongpham26.LaptopShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopShopApplication {
 
 	public static void main(String[] args) {
